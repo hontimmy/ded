ec80e96e3bce1d1d6b338d0322469d9244e1bc68
hontimmy <oyewoletimmy20@gmail.com>
2020-09-30 02:28:14 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <UsrMsg @inputData="updateMessage" /> <Results :msg="childData" />
+  </div>
+</template>
+
+<script>
+import UsrMsg from "./components/UsrMsg";
+import Results from "./components/Results";
+
+export default {
+  name: "App",
+  components: {
+    UsrMsg,
+    Results
+  },
+  data: function() {
+    return {
+      childData: ""
+    };
+  },
+  methods: {
+    updateMessage(variable) {
+      this.childData = variable;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
